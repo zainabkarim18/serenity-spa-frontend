cf5c498c7d97558728c4aa6ef5e6bad7df9b8be7
Maryam <maryammohsen202002861@gmail.com>
2024-08-18 16:50:00 +0300

    home page, auth service

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
 const App = () => {
   return (
-  <>
-  <h1>Hello world!</h1>
-  <h1>Hello world!</h1>
-  <h1>Hello world!</h1>
-  
-  </>
-
-)};
+    <>
+      <div className="home-content">
+        <h4>Hello, welcome to</h4>
+        <h1 className="glowing-title">
+          Serenity <span>Spa</span>
+        </h1>
+        <h3>Where you will feel most Beautiful</h3>
+        <br /><br /><br />
+        <a className="appoint-button" href="services.html">Make an Appointment</a>
+      </div>
+    </>
+  );
+};
 
 export default App;
