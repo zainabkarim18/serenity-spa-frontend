7ed7c626ea1f0ef70b777a718f71d80d52568c7b
Maryam <maryammohsen202002861@gmail.com>
2024-08-19 15:25:27 +0300

    styling the app.jsx, adding the footer component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ServiceList from "./components/service/ServiceList";
 import NavBar from "./components/partials/NavBar";
 import SignupForm from './components/auth/SignUpForm';
 import SigninForm from './components/auth/SignInForm';
+import Footer from './components/partials/Footer';
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser());
@@ -17,25 +18,30 @@ const App = () => {
   }
 
   return (
-    <>
+    <div id="root">
+      <div className="background-wrapper"></div>
+
       <NavBar user={user} handleSignout={handleSignout} />
 
-      <div className="home-content">
-        <h4>Hello, welcome to</h4>
-        <h1 className="glowing-title">
-          Serenity <span>Spa</span>
-        </h1>
-        <h3>Where you will feel most Beautiful</h3>
-        <br /><br /><br />
-        <a className="appoint-button" href="/services">Make an Appointment</a>
-      </div>
+      <main>
+        <div className="home-content">
+          <h4>Hello, welcome to</h4>
+          <h1 className="glowing-title">
+            Serenity <span>Spa</span>
+          </h1>
+          <h3>Where you will feel most Beautiful</h3>
+          <br /><br /><br />
+          <a className="appoint-button" href="/services">Make an Appointment</a>
+        </div>
+        
+        <Routes>
+          <Route path="/services" element={<ServiceList />} />
+          <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+          <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+        </Routes>
+      </main>
 
-      <Routes>
-        <Route path="/services" element={<ServiceList />} />
-        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
-        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
-      </Routes>
-    </>
+    </div>
   );
 };
 
