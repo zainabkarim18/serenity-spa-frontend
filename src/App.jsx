48969634ca6db9b556dcfd7b86e8d256534890a6
danahrb <danahbahar@gmail.com>
2024-08-20 15:04:15 +0300

    added CRUD fun for the review

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-
-// import * as serviceService from './services/serviceService';
-import { Routes, Route} from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { useState} from 'react';
-import authService from './services/authService'
+import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
+import authService from './services/authService';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Components
 import ServiceList from "./components/service/ServiceList";
@@ -19,36 +17,42 @@ const App = () => {
   const handleSignout = () => {
     authService.signout();
     setUser(null);
-  }
+  };
 
   return (
     <div id="root">
+      {/* Background wrapper */}
       <div className="background-wrapper"></div>
 
+      {/* Navigation Bar */}
       <NavBar user={user} handleSignout={handleSignout} />
 
-      <main>
-        <div className="home-content">
-          <h4>Hello, welcome to</h4>
-          <h1 className="glowing-title">
+      {/* Main Content */}
+      <main className="container mt-4">
+        {/* Hero Section */}
+        <div className="text-center mb-4">
+          <h4 className="mb-3">Hello, welcome to</h4>
+          <h1 className="display-3 glowing-title">
             Serenity <span>Spa</span>
           </h1>
-          <h3>Where you will feel most Beautiful</h3>
-          <br /><br /><br />
-          <a className="appoint-button" href="/services">Make an Appointment</a>
+          <h3 className="mb-4">Where you will feel most Beautiful</h3>
+          <Link className="btn btn-primary btn-lg" to="/services">
+            Make an Appointment
+          </Link>
         </div>
-        
+
+        {/* Routes */}
         <Routes>
           <Route path="/services" element={<ServiceList />} />
           <Route path="/services/:id" element={<ServiceDetail />} />
           <Route path="/signup" element={<SignupForm setUser={setUser} />} />
           <Route path="/signin" element={<SigninForm setUser={setUser} />} />
-          
         </Routes>
       </main>
 
+      {/* Footer */}
+      <Footer />
     </div>
-
   );
 };
 
