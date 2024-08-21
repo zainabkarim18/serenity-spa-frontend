e15217e341c264b8f495ce3dd5fa3902c439e4fd
Maryam <maryammohsen202002861@gmail.com>
2024-08-21 16:22:36 +0300

    fixing serviceList and serviceDetails errors

--- src/components/service/ServiceList.jsx
+++ src/components/service/ServiceList.jsx
@@ -1,72 +1,34 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import * as serviceService from "../../services/serviceService";
-import ServiceEditForm from "../service/ServiceEditForm";
-import ServiceDetail from "../service/ServiceDetail";
-import ServiceForm from "./ServiceForm";
 
 const ServiceList = () => {
-  const [serviceList, setServiceList] = useState([]);
-  const [isEdit, setIsEdit] = useState(false);
-  const [servicetoEdit, setServiceToEdit] = useState({});
-  const [showForm, setShowForm] = useState(false);
-
-  const changeEdit = () => {
-    setIsEdit(!isEdit);
-  };
+  const [services, setServices] = useState([]);
 
   useEffect(() => {
     const fetchServices = async () => {
       try {
-        const services = await serviceService.index();
-        if (services.error) {
-          throw new Error(services.error);
-        }
-        setServiceList(services);
-        console.log(services);
-      } catch (err) {
-        console.log(err);
+        const serviceData = await serviceService.index();
+        setServices(serviceData);
+      } catch (error) {
+        console.error("Error fetching services:", error);
       }
     };
     fetchServices();
   }, []);
 
-  const handleDeleteService = async (id) => {
-    const deletedService = await serviceService.remove(id);
-    setServiceList(serviceList.filter((ser) => ser._id !== deletedService._id));
-  };
-  const handleFormSubmit = () => {
-    setShowForm(false);
-  };
-
-  const services = serviceList.map((ser, index) => (
-    <li key={ser._id}>
-      <ServiceDetail
-        {...ser}
-        id={ser._id}
-        key={index}
-        setIsEdit={changeEdit}
-        setServiceToEdit={setServiceToEdit}
-        handleDeleteService={handleDeleteService}
-      />
-    </li>
-  ));
-
   return (
     <div>
-      {showForm && <ServiceForm onFormSubmit={handleFormSubmit} />}
-      <button onClick={() => setShowForm(!showForm)}>Create New Service</button>
-      <div>
-        {isEdit && servicetoEdit ? (
-          <ServiceEditForm setIsEdit={changeEdit} serviceID={servicetoEdit} />
-        ) : (
-          ""
-        )}
-        <div>
-          <h1>Service List</h1>
-          {services}
-        </div>
-      </div>
+      <h1>Service List</h1>
+      <ul>
+        {services.map((service) => (
+          <li key={service._id}>
+            <Link to={`/services/${service._id}`}>
+              <h2>{service.name}</h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
