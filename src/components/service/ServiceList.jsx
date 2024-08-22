b68ef425cb46ce61caf3da00ebaccd637a4c5a64
zainabkarim18 <zainabkarim18@github.com>
2024-08-22 09:45:51 +0300

    updated service list

--- src/components/service/ServiceList.jsx
+++ src/components/service/ServiceList.jsx
@@ -1,94 +1,24 @@
 import { useEffect, useState } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import * as serviceService from "../../services/serviceService";
-import ServiceEditForm from '../service/ServiceEditForm';
-import ServiceDetail from '../service/ServiceDetail';
-import ServiceForm from './ServiceForm';
-import 'bootstrap/dist/css/bootstrap.min.css';
 const ServiceList = () => {
-  const [serviceList, setServiceList] = useState([]);
-  const [isEdit, setIsEdit] = useState(false);
-  const [servicetoEdit, setServiceToEdit] = useState({});
-  const [showForm, setShowForm] = useState(false);
-//   const [services, setServices] = useState(null);
-  const navigate = useNavigate(); // Add useNavigate hook
-
-  const changeEdit = () => {
-    setIsEdit(!isEdit);
-  };
-
-  const fetchServices = async () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    const fetchServices = async () => {
       try {
         const serviceData = await serviceService.index();
-        setServiceList(serviceData);
-        console.log(serviceList);
-        
+        setServices(serviceData);
       } catch (error) {
         console.error("Error fetching services:", error);
       }
     };
-
-//   const fetchServices = async () => {
-//     try {
-//       const services = await serviceService.index();
-//       if (!services) {
-//         throw new Error(services);
-// // =======
-// //   const [services, setServices] = useState([]);
-
-// //   useEffect(() => {
-// //     const fetchServices = async () => {
-// //       try {
-// //         const serviceData = await serviceService.index();
-// //         setServices(serviceData);
-// //       } catch (error) {
-// //         console.error("Error fetching services:", error);
-// // >>>>>>> main
-//       }
-//       setServiceList(services);
-//       console.log(services);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-  useEffect(() => {
     fetchServices();
   }, []);
-
-  const handleDeleteService = async (id) => {
-    const deletedService = await serviceService.remove(id);
-    setServiceList(serviceList.filter((ser) => ser._id !== deletedService._id));
-  };
-
-  const handleFormSubmit = () => {
-    setShowForm(false);
-    fetchServices();
-  };
-
-//   const services = serviceList.map((ser, index) => (
-//     <li key={ser._id}>
-//       <ServiceDetail
-//         {...ser}
-//         id={ser._id}
-//         key={index}
-//         setIsEdit={changeEdit}
-//         setServiceToEdit={setServiceToEdit}
-//         handleDeleteService={handleDeleteService}
-//       />
-//     </li>
-//   ));
-
   return (
     <div className="service">
-      <div className="add-service-button-container">
-        <Link to="/services/new">
-        <button className="add-service-button">Add Service</button>
-        </Link>
-    </div>
       <div className="title">
         <h2>Our Services</h2>
       </div>
-      
       <div className="box">
         {services.map((service) => (
           <div className="card" key={service._id}>
@@ -99,9 +29,6 @@ const ServiceList = () => {
               <p style={{ textAlign: "center" }}>
                 <Link to={`/services/${service._id}`} className="booking-button">More</Link>
               </p>
-
-  
-
             </div>
           </div>
         ))}
@@ -109,5 +36,17 @@ const ServiceList = () => {
     </div>
   );
 };
-
 export default ServiceList;
+
+
+
+
+
+
+
+
+
+
+
+
+
